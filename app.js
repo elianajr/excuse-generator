f0417fc4a2d9ad6ecd711896698002817a25167f
Eliana <elianajr1@gmail.com>
2021-09-13 21:27:59 +0200

    excuse generator

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+window.onload = () => {
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  
+  console.log("Hello Rigo from the console!");
+};
+
+
+let generateExcuse = () => {
+
+  let pronoun = ['A', 'The'];
+  let subject = ['jogger', 'racoon', 'dog', 'driver', 'comedian', 'pincone'];
+  let action = ['took my', 'threw my', 'yelled at my', 'stoled my', 'bit my'];
+  let possesion = ['homework', 'toe', 'car', 'shoe'];
+  let where = ['on the street', 'in my house', 'in my driveway'];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subjIndex = Math.floor(Math.random() * subject.length);;
+  let actionIndex = Math.floor(Math.random() * action.length);;
+  let possesionIndex = Math.floor(Math.random() * possesion.length);;
+  let whereIndex = Math.floor(Math.random() * where.length);;
+
+  return pronoun[proIndex] + ' ' + subject[subjIndex] + ' ' + action[actionIndex] + ' ' + possesion[possesionIndex] + ' ' + where[whereIndex];
+};
